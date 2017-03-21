f97a8845b3ac1698e8b69e9131416058f5dde47f
gyanendra1063 <gyan.maurya1063@gmail.com>
2017-03-21 12:13:40 +0530

    Update NaiveBayes.py

--- NaiveBayes.py
+++ NaiveBayes.py
@@ -1,5 +1,13 @@
 import csv
 
+def list_words(text):
+    words = []
+    words_tmp = text.lower().split()
+    for p in words_tmp:
+        if p not in words and len(p) > 2:
+            words.append(p)
+    return words
+
 def training(texts):
     c_words ={}
     c_categories ={}
@@ -10,7 +18,7 @@ def training(texts):
 	#claculate text count and category wise count of emails
     for t in texts:
         c_texts = c_texts + 1
-	print t[1]
+	#print t[1]
         if t[1] not in c_categories:
             c_categories[t[1]] = 1
         else:
@@ -33,17 +41,18 @@ def training(texts):
                for c in c_categories:
                    c_words[p][c] = 0
             c_words[p][t[1]] = c_words[p][t[1]] + 1
-    with open("mtd.csv","a") as out:
-    	for p in c_words :
-	    print (p , c_words[p]["spam"] , c_words[p]["nospam"])
+    ###with open("mtd.csv","a") as out:
+    	###for p in c_words :
+	   ### print (p , c_words[p]["spam"] , c_words[p]["nospam"])
     print ("total spam mails=" ,c_spam)
     print ("total ham mails=" ,c_ham)
-	
+    print (c_categories["spam"] , c_categories["nospam"])
+    print (c_texts, c_tot_words)
     return (c_words, c_categories, c_texts, c_tot_words)
 
 
 def classifier(subject_line, c_words, c_categories, c_texts, c_tot_words):
-    category =""
+    category ="spam"
     category_prob = 0
 
     for c in c_categories:
@@ -66,24 +75,22 @@ def classifier(subject_line, c_words, c_categories, c_texts, c_tot_words):
 
 
 if __name__ == "__main__":
-
+    #### call to main Training procedure ####
     with open('dataham.csv') as f:
         subjects = dict(csv.reader(f, delimiter=','))
         p,c,t,tp = training(subjects.items())
-    
-	#First Test
-        clase = classifier("Available on Term Life - Free,nospam",p,c,t,tp)
-        print("Result: {0} ".format(clase))
 
 
-    #Second Test
+    #### main Test procedure  #####
     with open("test.csv") as f:
         correct = 0
+	total=0
         tests = csv.reader(f)
         for subject in tests:
             clase = classifier(subject[0],p,c,t,tp)
 	    print (clase[0],clase[1],subject[1])            
 	    if clase[0] == subject[1]:
                 correct += 1
-        print("Efficiency {0} of 10".format(correct))
-
+	    total+=1
+	eff=float((float(correct)/float(total)))*100	
+        print("Efficiency is {0}".format(eff))
